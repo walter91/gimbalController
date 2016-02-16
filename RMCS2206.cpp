718c5c549118123839bd19665bf172d8db7840a5
walter91 <waltermcoe@gmail.com>
2016-02-16 06:26:17 -0700

    Initial Commit
    This is a private repository for my capstone project. Using an Arduino
    DUE and Rhino Motion Controls RMCS-220X motor,  we are driving a novel
    gimbal system.

---
+++ RMCS2206.cpp
@@ -0,0 +1,85 @@
+#include <Arduino.h>
+#include "RMCS2206.h"
+
+RMCS2206::RMCS2206(char dof, int serialNumber)
+{
+	/*
+	Initialize an RMCS2206 object as either 'A' for azimuth
+	or 'E' for elevation. Calculations for velocity, acceleration
+	and position will be based on this characteristic.
+	*/
+	
+	_dof = dof;
+	_serialNumber = serialNumber;
+	
+	if(_dof == 'A')
+		_degPerEnc = 0.2;
+	else if(_dof == 'E')
+		_degPerEnc = 0.2;
+	else
+		_faultFlag = 1;
+	
+//	return(_faultFlag);
+	
+}
+
+int RMCS2206::begin()
+{
+	/*
+	Initialize serial communication.
+	Then set all parameters for the motor via serial communication.
+	'M' for max motor speed (0 - 255)
+	'B' for P gain term (0 - 32767)
+	'C' for I gian term (0 - 32767)
+	'D' read/write speed dampening (0 - 255)
+	'P' read/write encoder position (0 - 2147483647)
+	*/
+	
+	switch(_serialNumber)
+	{
+		case 1:
+			#define serial Serial1
+			break;
+		case 2:
+			#define serial Serial2
+			break;
+		case 3:
+			#define serial Serial3
+			break;
+		default:
+			_faultFlag = 1;
+			break;
+	}
+	
+	serial.begin(9600);
+	
+	
+	return(_faultFlag);
+	
+}
+
+void RMCS2206::set_position(float deg)
+{
+	//
+	
+}
+
+void RMCS2206::set_speed(float degPerSec)
+{
+	//
+	
+}
+
+void RMCS2206::move(float deg)
+{
+	//Move a relative encoder count using 'R' command
+	
+	serial.print("R"); serial.println(int(deg/_degPerEnc));
+	
+}
+
+
+
+
+
+
---
+++ RMCS2206.h
@@ -0,0 +1,32 @@
+/*
+RMCS2206.h - Library for using the L-3 Communications NimbleGimble Prototype
+
+Created by Team Align (#16 Capstone 2015-2016)
+Use of this code without written permission is strictly prohibited.
+
+*/
+
+#ifndef RMCS2206_h
+#define RMCS2206_h
+
+class RMCS2206
+{
+	public:
+	
+		RMCS2206(char dof, int serialNumber);
+		int begin();
+		void set_position(float deg);
+		void set_speed(float degPerSec);
+		void move(float deg);
+		
+		
+	private:
+		
+		bool _faultFlag = 0;
+		int _serialNumber;
+		float _degPerEnc;
+		int _dof;
+	
+};
+
+#endif
